8c83f9a19c498a223866f63054fef7fff66a69af
mecab <mecab@misosi.ru>
2022-05-27 02:28:40 +0900

    Support Firefox

--- app/scripts/background.ts
+++ app/scripts/background.ts
@@ -2,6 +2,51 @@ import browser from 'webextension-polyfill';
 import * as constants from './constants';
 import * as InjectionUtil from './InjectionUtil';
 
+type AnyFunc = (...args: any[]) => any;
+
+function wrapFuncToCode<T extends () => any>(func: T): string;
+function wrapFuncToCode<T extends AnyFunc>(func: T, args: Parameters<T>): string;
+function wrapFuncToCode<T extends AnyFunc>(func: T, args: any[] = []): string
+{
+    return `(${func.toString()}).apply(this, ${JSON.stringify(args)})`;
+}
+
+async function executeScript<T extends () => any>(tabId: number, injection: { func: T }): Promise<ReturnType<T>>;
+async function executeScript<T extends AnyFunc>(tabId: number, injection: { func: T, args: Parameters<T> }): Promise<ReturnType<T>>;
+async function executeScript(tabId: number, injection: { file: string }): Promise<any>;
+async function executeScript<T extends AnyFunc>(tabId: number, { func, args, file }: { func?: T, args?: any, file?: string }): Promise<ReturnType<T> | any> {
+    let result: any;
+    if (process.env['VENDOR'] === 'firefox') {
+        (
+            [ result ] = await browser.tabs.executeScript(tabId, {
+                ... func ? { code : wrapFuncToCode(func, args) } : {},
+                ... file ? { file } : {},
+            })
+        );
+    } else {
+        (
+            [ { result } = { result: undefined } ] = await browser.scripting.executeScript({
+                target: { tabId },
+                ... func ? { func, args } : {},
+                ... file ? { files: [ file ] } : {},
+            })
+        );
+    }
+    
+    return result as ReturnType<T>;
+}
+
+async function insertCSS(tabId: number, file: string) {
+    if (process.env['VENDOR'] === 'firefox') {
+        browser.tabs.insertCSS(tabId, { file });
+    } else {
+        browser.scripting.insertCSS({
+            target: { tabId },
+            files: [ file ],
+        });
+    }
+}
+
 browser.contextMenus.create({
     id: constants.CONTEXT_MENU_ID,
     title: '坪単価を計算する',
@@ -15,24 +60,17 @@ browser.contextMenus.onClicked.addListener(async (_, tab) => {
         return;
     }
 
-    const [ { result: isInjected } = { result: false } ] = await browser.scripting.executeScript(
-        {
-            target: { tabId },
-            func: InjectionUtil.isInjected,
-        }
-    );
+    const isInjected = await executeScript(tabId, {
+        func: InjectionUtil.isInjected,
+    });
+    
 
     if (!isInjected) {
         console.log('injecting scripts');
-        browser.scripting.insertCSS({
-            target: { tabId },
-            files: ['/styles/contentscript.css'],
-        });
 
-        await browser.scripting.executeScript({
-            target: { tabId },
-            files: ['/scripts/contentscript.js'],
-        });
+        insertCSS(tabId, '/styles/contentscript.css');
+        
+        await executeScript(tabId, { file: '/scripts/contentscript.js' });
     }
 
     browser.tabs.sendMessage(tabId, { event: constants.EV_TRIGGER_TSUBO_CALCULATION });
--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -6,18 +6,18 @@ import { markAsInjected, isInjected } from './InjectionUtil';
 import { UserInterface } from './UserInterface';
 
 if (isInjected()) {
-    throw new Error('Already injected. Stopping execution for safety.');
+    console.warn('Already injected. Will do nothing.')
+} else {
+    markAsInjected();
+
+    const event = new EventEmitter();
+    const ui = new UserInterface();
+
+    browser.runtime.onMessage.addListener((e: { event: string, data: any }) => {
+        event.emit(e.event, e.data);
+    });
+
+    event.addListener(constants.EV_TRIGGER_TSUBO_CALCULATION, () => {
+        ui.contextMenuFiredHandler();
+    });
 }
-
-markAsInjected();
-
-const event = new EventEmitter();
-const ui = new UserInterface();
-
-browser.runtime.onMessage.addListener((e: { event: string, data: any }) => {
-    event.emit(e.event, e.data);
-});
-
-event.addListener(constants.EV_TRIGGER_TSUBO_CALCULATION, () => {
-    ui.contextMenuFiredHandler();
-});
